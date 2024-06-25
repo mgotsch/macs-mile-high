f76d374206962799e11537d8e0b26e0768740d93
Mac Gotsch <mac@acadiantdigital.com>
2024-06-25 16:53:03 -0600

    Working initial version of the map populated with location data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import MyMap from './components/MyMap';
 export default function App() {
-  const mapboxToken = "pk.eyJ1IjoibWdvdHNjaCIsImEiOiJjbHhjcTg2dzgwNWhyMmtweml1YTdsYTdtIn0.aravVVUSaFlsjEZmdx503A";
 
   return (
     <MyMap/>
--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -1,28 +1,52 @@
-import * as React from 'react';
-import Map, {Marker} from 'react-map-gl';
+import  {useState, useEffect} from 'react';
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import markerData from '../data/markers.json';
 
-const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_API_KEY;
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 
 export default function MyMap() {
+  
+  const [markers, setMarkers] = useState([]);
+  // Set bounds to Denver, Colorado.
+  const bounds = [
+    [-106, 39.5],
+    [-104, 40]
+  ];  
+  
+  useEffect(() => {
+    // Directly set the imported JSON data
+    console.log(markerData);
+    setMarkers(markerData.features);
+  }, []);
 
-  const [viewState, setViewState] = React.useState({
-    longitude: -98.3509,
-    latitude: 39.8283,
-    zoom: 3.5,
-    pitch: 0,
-    bearing: 0,
-  });
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      center: [-105, 39.75],
+      zoom: 10.5,
+      style: 'mapbox://styles/mgotsch/clxtlfu2r00o301rndnipcx45',
+      maxBounds: bounds
+    });
+
+    markers.forEach(marker => {
+      // console.log(marker);
+      const locationType = marker.properties.location.loc_type.toLowerCase();
+      const el = document.createElement('div');
+      el.className = `marker marker-${locationType}`;
+      // el.style.backgroundImage = `url(./icons/${locationType}.png)`;
+     
+      new mapboxgl.Marker(el)
+        .setLngLat([marker.geometry.coordinates[0], marker.geometry.coordinates[1]])
+        .setPopup(new mapboxgl.Popup().setHTML(
+          `<div class="pop-up"><h3>${marker.properties.location.name}</h3><p>${marker.properties.location.address}</p></div>`
+        ))
+        .addTo(map);
+    });
+
+  }, [markers]);
   
   return (
-    <Map
-      {...viewState}
-      onMove={evt => setViewState(evt.viewState).then(console.log(evt.viewState))}
-      style={{width: "100vw", height: "100vh"}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxAccessToken={MAPBOX_TOKEN}
-    >
-      
-    </Map>
+    <div id="map"style={{width: "100vw", height: "100vh"}}></div>
   );
 }
